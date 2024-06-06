842c6e6da6bc8e17e231327232c0339697f8145e
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2024-06-06 18:12:18 +0530

    Deleted docker compose

--- src/dto/tenant.dto.ts
+++ src/dto/tenant.dto.ts
@@ -1,6 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+class JwtConfiguration {
+  @ApiProperty() accessTokenKeyID: string;
+  @ApiProperty() refreshTokenTimeToLiveInMinutes: number;
+  @ApiProperty() timeToLiveInSeconds: number;
+  @ApiProperty() idTokenKeyID: string;
+}
+
 export class CreateTenantDto {
   @ApiProperty() name: string;
-  @ApiProperty() 
+  @ApiProperty() jwtConfiguration: JwtConfiguration;
+  @ApiProperty() data?: any; // additional data
 }
