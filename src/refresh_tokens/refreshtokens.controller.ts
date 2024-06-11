83fe661107bcbf25f56c732b42708239aec0ece5
Ashu463 <ashukasaudhan971@gmail.com>
2024-06-11 11:45:37 +0530

    Delete apis added in refresh token

--- src/refresh_tokens/refreshtokens.controller.ts
+++ src/refresh_tokens/refreshtokens.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Headers, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Header, Headers, Param, Post } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { RefreshTokensService } from "./refreshtokens.service";
 import { refreshCookiesDTO, refreshDTO } from "./refreshToken.dto";
@@ -24,8 +24,25 @@ export class RefreshTokensController{
         return this.refreshService.retrieveByUserID(userId); 
     }
 
+
     @Delete('/refresh')
-    async deleteEntireApp(@Headers('applicationId') appId : string){
-        return this.deleteEntireApp(appId)
+    async deleteViaApp(@Param('applicationId') applicationId : string){
+        return this.refreshService.deleteViaAppID(applicationId);
+    }
+    @Delete('/refresh')
+    async deleteViaUserID(@Param('usersId') usersId : string, @Param('token') token ?: string){
+        return this.refreshService.deleteViaUserID(usersId, token);
+    }
+    @Delete('/refresh')
+    async deleteViaUserAndAppID(@Param('userId') userId : string, @Param('applicationId') applicationsId : string, token ?: string){
+        return this.refreshService.deleteViaUserAndAppID(userId, applicationsId)
+    }
+    @Delete('/refresh/:tokenId')
+    async deleteViaTokenID(@Param('tokenId') id : string){
+        return this.refreshService.deleteViaTokenID(id);
+    }
+    @Delete('/refresh')
+    async deleteViaToken(@Param('token') token : string){
+        return this.refreshService.deleteViaToken(token)
     }
 }
--- src/refresh_tokens/refreshtokens.service.ts
+++ src/refresh_tokens/refreshtokens.service.ts
@@ -1,4 +1,4 @@
-import { BadGatewayException, BadRequestException, Body, Header, Headers, Injectable, Logger, Options } from "@nestjs/common";
+import { BadGatewayException, BadRequestException, Body, Header, Headers, Injectable, Logger, Options, Param } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { PrismaService } from "src/prisma/prisma.service";
 import { refreshCookiesDTO, refreshDTO } from "./refreshToken.dto";
@@ -41,7 +41,7 @@ export class RefreshTokensService {
                     return {
                         success: true,
                         message: 'access token generated successfully!',
-                        item
+                        data : item
                     }
                 } else {
                     throw new BadRequestException({
@@ -80,7 +80,9 @@ export class RefreshTokensService {
                 })
             } else {
                 return {
-                    refreshToken,
+                    success : true,
+                    message : 'refresh token generated successfully',
+                    data : refreshToken,
                 }
             }
         } catch (error) {
@@ -112,7 +114,9 @@ export class RefreshTokensService {
                 })
             } else {
                 return {
-                    refreshToken,
+                    success : true,
+                    message : 'refresh token generated successfully',
+                    data : refreshToken,
                 }
             }
         } catch (error) {
@@ -124,8 +128,174 @@ export class RefreshTokensService {
         }
     }
 
-    // deleting refresh tokens 
-    async deleteEntireApp(application : string){
-        
+    // deleting all refresh token associated with one application
+    async deleteViaAppID(applicationId : string, token ?: string){
+        if(!applicationId){
+            throw new BadRequestException({
+                success : false,
+                message : 'please send a valid application ID'
+            })
+        }
+        try{
+            const appId = await this.prismaService.refreshToken.findUnique({ where : {
+                applicationsId : applicationId ,
+                token : token
+            }})
+            if(appId){
+                await this.prismaService.refreshToken.deleteMany({ where : {
+                    applicationsId : applicationId 
+                }})
+                return {
+                    success : true,
+                    message : 'all refresh tokens deleted successfully with the given application id'
+                }
+            }else{
+                throw new BadGatewayException({
+                    success : false,
+                    message : 'application id not found'
+                })
+            }
+            
+        }catch(error){
+            this.logger.log(error)
+            throw new BadGatewayException({
+                success : false, 
+                message : 'error occured while deleting all refresh token from given application id'
+            })
+        }
+    }
+    // deleting all refresh token associated with a userID
+    async deleteViaUserID(usersId : string, token ?: string){
+        if(!usersId){
+            throw new BadRequestException({
+                success : false,
+                message : 'please send a valid user id'
+            })
+        }
+        try{
+            const userId = await this.prismaService.refreshToken.findUnique({ where : {
+                usersId : usersId,
+                token : token
+            }})
+            if(userId){
+                await this.prismaService.refreshToken.deleteMany({ where : {
+                    usersId: usersId
+                }})
+                return {
+                    success : true,
+                    message : 'all refresh token is deleted successfully with the help of given user id'
+                }
+            }else{
+                throw new BadGatewayException({
+                    success : false,
+                    message : 'unable to find refresh token with provided credentials'
+                })
+            }
+        }catch(error){
+            this.logger.log(error)
+            throw new BadGatewayException({
+                success : false,
+                message : 'error occured while deleting refresh token from given user id'
+            })
+        }
+    }
+    // deleting refersh token via given userid and applicationID
+    async deleteViaUserAndAppID( userId : string, applicationsId : string, token ?: string){
+        if(!userId || !applicationsId){
+            throw new BadGatewayException({
+                success : false,
+                message : 'please send userId and applicationId both'
+            })
+        }
+        try{
+            const userID = await this.prismaService.refreshToken.findUnique({ where : {usersId : userId, token : token}})
+            const appId = await this.prismaService.refreshToken.findUnique({ where : {applicationsId : applicationsId, token : token}})
+            if(userID && appId){
+                await this.prismaService.refreshToken.delete({ where : {
+                    usersId : userId,
+                    applicationsId : applicationsId,
+                    token : token
+                }})
+            return {
+                success : true,
+                message : 'refresh token deleted with provided application ID and user ID'
+            }
+            }else{
+                throw new BadGatewayException({
+                    success : false,
+                    message : 'anyone among userId or applicationsId is not found'
+                })
+            }
+        }catch(error){
+            this.logger.log(error)
+            throw new BadGatewayException({
+                success : false,
+                message : 'error occured deleting refresh tokens while userId and applicationId both are given'
+            })
+        }
+    }
+    // deleting refersh token via given tokenID
+    async deleteViaTokenID( id : string){
+        if(!id){
+            throw new BadRequestException({
+                success : false,
+                message : 'please send a valid token id'
+            })
+        }
+        try{
+            const ref_token = await this.prismaService.refreshToken.findUnique({ where : {
+                id : id
+            }})
+            if(ref_token){
+                await this.prismaService.refreshToken.delete({ where : {id}})
+                return {
+                    success : true,
+                    message : 'refresh token is deleted successfully with the help of given token id'
+                }
+            }else{
+                throw new BadGatewayException({
+                    success : false,
+                    message : 'unable to find refresh token with provided credentials'
+                })
+            }
+        }catch(error){
+            this.logger.log(error)
+            throw new BadGatewayException({
+                success : false,
+                message : 'error occured while deleting refresh token from given token id'
+            })
+        }
+    }
+    // deleting refresh token via given refresh token
+    async deleteViaToken(token : string){
+        if(!token){
+            throw new BadRequestException({
+                success : false,
+                message : 'please send a valid token'
+            })
+        }
+        try{
+            const ref_token = await this.prismaService.refreshToken.findUnique({ where : {
+                token : token
+            }})
+            if(ref_token){
+                await this.prismaService.refreshToken.delete({ where : {token}})
+                return {
+                    success : true,
+                    message : 'refresh token is deleted successfully with the help of given token'
+                }
+            }else{
+                throw new BadGatewayException({
+                    success : false,
+                    message : 'unable to find refresh token with provided credentials'
+                })
+            }
+        }catch(error){
+            this.logger.log(error)
+            throw new BadGatewayException({
+                success : false,
+                message : 'error occured while deleting refresh token from given token string'
+            })
+        }
     }
 }
