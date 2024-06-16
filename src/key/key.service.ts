5bc204296c13112a48a1d52be155dd0065413c99
Ashu463 <ashukasaudhan971@gmail.com>
2024-06-16 23:36:11 +0530

    conflicts resolved

--- src/key/key.service.ts
+++ src/key/key.service.ts
@@ -2,13 +2,9 @@ import { BadGatewayException, BadRequestException, Body, Headers, HttpException,
 import { STATUS_CODES } from "http";
 import { generateKeyDTO, updateDTO } from "src/key/key.dto";
 import { PrismaService } from "src/prisma/prisma.service";
-<<<<<<< HEAD
-import * as jose from 'node-jose';
-=======
 import jose from 'node-jose';
 import { JWK } from 'jose';
 
->>>>>>> refresh-token-api
 
 @Injectable()
 export class KeyService {
@@ -122,89 +118,14 @@ export class KeyService {
             })
         }
     }
-<<<<<<< HEAD
-    async generateKey(uuid: string, key: generateKeyDTO) {
-        if (!uuid) {
-=======
     async generateKey(uuid :string, data : generateKeyDTO){
         console.log(data)
         if(!uuid){
->>>>>>> refresh-token-api
             throw new BadRequestException({
                 success: false,
                 message: 'uuid is either not given or is invalid'
             })
         }
-<<<<<<< HEAD
-        const { algorithm, name, length, issuer} = key ;
-        const keyStore = jose.JWK.createKeyStore();
-        const keyStore2 = jose.JWK.createKeyStore();
-        const keyStore3 = jose.JWK.createKeyStore();
-
-        try {
-            if (algorithm === 'RS256') {
-                await keyStore.generate("RSA", 2048, { alg: "HS256", use: "sig" }).then((result) => {
-                    const rskey = JSON.stringify(keyStore.toJSON(true), null, 2);
-                    this.logger.log("RS key generated successfully")
-                });
-                const jwks = keyStore.toJSON(true);
-                jwks.keys.forEach(key => {
-                    key.id = uuid;
-                    key.issuer = issuer;
-                    key.length = length;
-                    key.userName = name;
-                    key.type = 'RSA'
-                });
-                return {
-                    success: true,
-                    message: 'key generated successfully',
-                    data : jwks
-                }
-            } else if (algorithm === "ES256") {
-                await keyStore2.generate('EC', 'P-256', { alg: 'ES256', use: 'sig' }).then((result) => {
-                    const eckey = JSON.stringify(keyStore2.toJSON(true), null, 2);
-                    this.logger.log("EC key generated successfully")
-                });
-                console.log("eckey generate @@@@@@@@@@@@")
-                const jwks = keyStore2.toJSON(true);
-                jwks.keys.forEach(key => {
-                    key.id = uuid;
-                    key.issuer = issuer;
-                    key.length = length;
-                    key.userName = name;
-                    key.type = 'EC'
-                });
-                return {
-                    success: true,
-                    message: 'key generated successfully',
-                    data : jwks
-                }
-            } else {
-                await keyStore3.generate('oct', 256, { alg: 'HS256', use: 'sig' }).then((result) => {
-                    const hskey = JSON.stringify(keyStore3.toJSON(true), null, 2);
-                    this.logger.log("HS key generated successfully")
-                });
-                const jwks = keyStore3.toJSON(true);
-                jwks.keys.forEach(key => {
-                    key.id = uuid;
-                    key.userName = name;
-                    key.type = 'HMAC'
-                });
-                return {
-                    success: true,
-                    message: 'key generated successfully',
-                    data : jwks
-                }
-            }
-        } catch (error) {
-            throw new BadRequestException({
-                success: false,
-                message: 'error while generating key',
-            })
-        }
-    }
-
-=======
         const algorithm = data.algorithm;
         const name = data.name 
         const length = data.length 
@@ -286,5 +207,4 @@ export class KeyService {
         // }
     } 
    
->>>>>>> refresh-token-api
 }
