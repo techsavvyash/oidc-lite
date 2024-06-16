6d2cbd8ab26a6564dbb8b5921afeeb10066aaee9
Ashu463 <ashukasaudhan971@gmail.com>
2024-06-17 01:10:41 +0530

    key API generated

--- src/key/key.service.ts
+++ src/key/key.service.ts
@@ -3,6 +3,8 @@ import { STATUS_CODES } from "http";
 import { generateKeyDTO, updateDTO } from "src/key/key.dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import * as jose from 'node-jose';
+const jwkToPem = require('jwk-to-pem');
+
 
 @Injectable()
 export class KeyService {
@@ -118,77 +120,123 @@ export class KeyService {
     }
     async generateKey(uuid: string, key: generateKeyDTO) {
         if (!uuid) {
-            throw new BadRequestException({
-                success: false,
-                message: 'uuid is either not given or is invalid'
-            })
+          throw new BadRequestException({
+            success: false,
+            message: 'uuid is either not given or is invalid',
+          })
         }
-        const { algorithm, name, length, issuer} = key ;
+        const { algorithm, name, length, issuer } = key;
+        console.log(algorithm)
         const keyStore = jose.JWK.createKeyStore();
         const keyStore2 = jose.JWK.createKeyStore();
         const keyStore3 = jose.JWK.createKeyStore();
-
+    
         try {
-            if (algorithm === 'RS256') {
-                await keyStore.generate("RSA", 2048, { alg: "HS256", use: "sig" }).then((result) => {
-                    const rskey = JSON.stringify(keyStore.toJSON(true), null, 2);
-                    this.logger.log("RS key generated successfully")
+          if (algorithm === 'RS256') {
+            await keyStore
+              .generate('RSA', 2048, { alg: 'HS256', use: 'sig' })
+              .then(() => {
+                const rskey = JSON.stringify(keyStore.toJSON(true), null, 2);
+                this.logger.log('RS key generated successfully');
+              });
+            const jwks = keyStore.toJSON(true);
+            const publicKeyPem = jwkToPem(jwks.keys[0]);
+            const privateKeyPem = jwkToPem(jwks.keys[0], { private: true });
+            const key = await this.prismaService.key.create({
+              data: {
+                id: uuid,
+                algorithm,
+                name,
+                issuer,
+                kid: jwks.keys[0].kid,
+                privateKey: privateKeyPem,
+                publicKey: publicKeyPem,
+                type: 'RS',
+              },
+            });
+            this.logger.log(
+              'RS key generated successfully',
+              publicKeyPem,
+              privateKeyPem,
+            );
+    
+            return {
+              success: true,
+              message: 'key generated successfully',
+              data: jwks,
+            };
+          } else if (algorithm === 'ES256') {
+              await keyStore2
+              .generate('EC', 'P-256', { alg: 'ES256', use: 'sig' })
+              .then(() => {
+                  const eckey = JSON.stringify(keyStore2.toJSON(true), null, 2);
                 });
-                const jwks = keyStore.toJSON(true);
-                jwks.keys.forEach(key => {
-                    key.id = uuid;
-                    key.issuer = issuer;
-                    key.length = length;
-                    key.userName = name;
-                    key.type = 'RSA'
-                });
-                return {
-                    success: true,
-                    message: 'key generated successfully',
-                    data : jwks
-                }
-            } else if (algorithm === "ES256") {
-                await keyStore2.generate('EC', 'P-256', { alg: 'ES256', use: 'sig' }).then((result) => {
-                    const eckey = JSON.stringify(keyStore2.toJSON(true), null, 2);
-                    this.logger.log("EC key generated successfully")
-                });
-                console.log("eckey generate @@@@@@@@@@@@")
                 const jwks = keyStore2.toJSON(true);
-                jwks.keys.forEach(key => {
-                    key.id = uuid;
-                    key.issuer = issuer;
-                    key.length = length;
-                    key.userName = name;
-                    key.type = 'EC'
-                });
-                return {
-                    success: true,
-                    message: 'key generated successfully',
-                    data : jwks
-                }
-            } else {
-                await keyStore3.generate('oct', 256, { alg: 'HS256', use: 'sig' }).then((result) => {
-                    const hskey = JSON.stringify(keyStore3.toJSON(true), null, 2);
-                    this.logger.log("HS key generated successfully")
-                });
-                const jwks = keyStore3.toJSON(true);
-                jwks.keys.forEach(key => {
-                    key.id = uuid;
-                    key.userName = name;
-                    key.type = 'HMAC'
-                });
-                return {
-                    success: true,
-                    message: 'key generated successfully',
-                    data : jwks
-                }
-            }
-        } catch (error) {
+                console.log(jwks)
+                const publicKeyPem = jwkToPem(jwks.keys[0]);
+                const privateKeyPem = jwkToPem(jwks.keys[0], { private: true });
+                const key = await this.prismaService.key.create({
+              data: {
+                id: uuid,
+                algorithm,
+                name,
+                issuer,
+                kid: jwks.keys[0].kid,
+                privateKey: privateKeyPem,
+                publicKey: publicKeyPem,
+                type: 'EC',
+            },
+        });
+            this.logger.log(
+              'EC key generated successfully',
+              publicKeyPem,
+              privateKeyPem,
+            );
+    
+            return {
+              success: true,
+              message: 'key generated successfully',
+              data: jwks,
+            };
+          } else if (algorithm === 'HS256') {
+            await keyStore3
+              .generate('oct', 256, { alg: 'HS256', use: 'sig' })
+              .then(() => {
+                const hskey = JSON.stringify(keyStore3.toJSON(true), null, 2);
+                this.logger.log('HS key generated successfully');
+              });
+            const jwks = keyStore3.toJSON(true);
+            console.log(jwks);
+            const key = await this.prismaService.key.create({
+              data: {
+                id: uuid,
+                algorithm,
+                name,
+                issuer,
+                kid: jwks.keys[0].kid,
+                secret: jwks.keys[0].k,
+                type: 'HS',
+              },
+            });
+            this.logger.log('HS key generated successfully', jwks.keys[0].k);
+            return {
+              success: true,
+              message: 'key generated successfully',
+              data: jwks,
+            };
+          } else {
             throw new BadRequestException({
-                success: false,
-                message: 'error while generating key',
-            })
+              success: false,
+              message: 'Unknown algorithm provided',
+            });
+          }
+        } catch (error) {
+          this.logger.log('Error from generateKey', error);
+          throw new BadRequestException({
+            success: false,
+            message: 'error while generating key',
+          });
         }
-    }
+      }
 
 }
