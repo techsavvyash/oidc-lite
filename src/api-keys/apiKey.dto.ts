46b8f23145b11cf8d446ca5664aec94275c33a87
Debatreya <debatreyadas@gmail.com>
2024-06-19 08:17:14 +0530

    Validations : user, user-registration, api-key

--- src/api-keys/apiKey.dto.ts
+++ src/api-keys/apiKey.dto.ts
@@ -1,39 +1,107 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsBoolean, IsDate, IsEnum, IsOptional, IsString, IsUUID, IsUrl } from 'class-validator';
 
 class Endpoints {
-  @ApiProperty() url: string;
-  @ApiProperty() methods: string;
+
+  @ApiProperty()
+  @IsUrl() 
+  url: string;
+
+  @ApiProperty() 
+  @IsEnum(
+    ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD', 'CONNECT', 'TRACE'],
+    {
+      message: 'Invalid HTTP method. Must be one of GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD, CONNECT, TRACE'
+    }
+  ) //check1
+  methods: string;
 }
 
 export class Permissions {
-  @ApiProperty() endpoints: Endpoints[];
+
+  @ApiProperty() 
+  @IsArray()
+  endpoints: Endpoints[];
 }
 
 export class CreateApiKeyDto {
-  @ApiProperty() key?: string;
-  @ApiProperty() permissions?: Permissions;
-  @ApiProperty() metaData?: string | JSON;
-  @ApiProperty() tenantId?: string;
+
+  @ApiProperty() 
+  @IsString()
+  @IsOptional()
+  key?: string;
+
+  @ApiProperty() 
+  permissions?: Permissions;
+
+  @ApiProperty()
+  @IsOptional() 
+  metaData?: string | JSON;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional() 
+  tenantId?: string;
 }
 class UpdataPermissionsDto {
-  @ApiProperty() endpoints?: Endpoints[];
+
+  @ApiProperty() 
+  @IsArray()
+  @IsOptional()
+  endpoints?: Endpoints[];
 }
 export class UpdateApiKeyDto {
-  @ApiProperty() key?: string;
-  @ApiProperty() permissions?: UpdataPermissionsDto;
-  @ApiProperty() metaData?: string | JSON;
+
+  @ApiProperty() 
+  @IsString()
+  @IsOptional()
+  key?: string;
+
+  @ApiProperty() 
+  @IsOptional()
+  permissions?: UpdataPermissionsDto;
+
+  @ApiProperty()
+  @IsOptional() 
+  metaData?: string | JSON;
 }
 
 class ApiKey {
-  @ApiProperty() id: string;
-  @ApiProperty() createdAt: Date;
-  @ApiProperty() updatedAt: Date;
-  @ApiProperty() keyManager: boolean;
-  @ApiProperty() permissions: Permissions;
-  @ApiProperty() metaData: string;
-  @ApiProperty() tenantsId: string;
+
+  @ApiProperty() 
+  @IsUUID()
+  id: string;
+
+  @ApiProperty() 
+  @IsDate()
+  createdAt: Date;
+
+  @ApiProperty() 
+  @IsDate()
+  updatedAt: Date;
+
+  @ApiProperty() 
+  @IsBoolean()
+  keyManager: boolean;
+
+  @ApiProperty() 
+  permissions: Permissions;
+
+  @ApiProperty() 
+  @IsString()
+  metaData: string;
+
+  @ApiProperty() 
+  @IsString()
+  tenantsId: string;
 }
 export class ApiKeyResponseDto {
-  @ApiProperty() message: string;
-  @ApiProperty() apiKey?: ApiKey;
+
+  @ApiProperty() 
+  @IsString()
+  message: string;
+
+  @ApiProperty() 
+  @IsOptional()
+  apiKey?: ApiKey;
 }
