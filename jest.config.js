e36182a10a7e743f42bb66caf09c54244ca94581
Ashu463 <ashukasaudhan971@gmail.com>
2024-06-23 01:06:23 +0530

    updated jest.config

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
   collectCoverageFrom: [
     "**/*.(t|j)s"
   ],
-  coverageDirectory: "../coverage",  // Ensures all reports are collected in a central coverage directory
+  coverageDirectory: "../coverage",
   coverageReporters: ["text", "lcov"],
   testEnvironment: "node"
 };
